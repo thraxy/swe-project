51d76068ac32e2f3a18642bd9b0495d8405f97d9
Nabil <100967334+thraxy@users.noreply.github.com>
2022-12-23 16:02:05 -0500

    Add files via upload

--- park/search.php
+++ park/search.php
@@ -6,22 +6,25 @@
 $server = "localhost";
 $user = "root";
 $pass = "";
-$db = "entries";
+$db = "parking_facilities_db";
 
 // connect to database
 $conn = mysqli_connect($server, $user, $pass, $db);
 
 // select database
-mysqli_select_db("entries");
+mysqli_select_db($conn, "parking_facilities_db");
 
 // get search term
 $searchTerm = $_POST['search'];
 
 // get matched data from database
-$query = mysqli_query("select * from entries", $connection);
-$db = mysqli_query("select * from address", $connection);
+// $query = mysqli_query($conn, "select * from entries");
+// $db = mysqli_query($conn, "select * from entries where name like '%".$searchTerm."%'");
+$query = mysqli_query($conn, "select * from entries where name like '%".$searchTerm."%' or address like '%".$searchTerm."%' or city like '%".$searchTerm."%' or state like '%".$searchTerm."%' or zipcode like '%".$searchTerm."%'");
 
 // fetch data
+while ($row = mysqli_fetch_assoc($query)){
+    ?>
 <span name: </span> 
     <?php echo $row['name']; ?>
 <span address: </span>
@@ -32,8 +35,11 @@ $db = mysqli_query("select * from address", $connection);
     <?php echo $row['state']; ?>
 <span zip: </span>
     <?php echo $row['zipcode']; ?>
+<?php 
+}
 
-mysqli_close($connection);
+mysqli_close($conn);
 
 ?>
+
 
